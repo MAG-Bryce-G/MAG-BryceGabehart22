01ee85614c28ac2b014fadd9f8b8573af842ac6c
MAG-Bryce-G <154249721+MAG-Bryce-G@users.noreply.github.com>
2024-05-29 12:08:56 -0400

    Update magtraining.py

--- magtraining.py
+++ magtraining.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def training():
     #construct your message here
-
-    return message
+    render_template("index.html")
+    return 200
